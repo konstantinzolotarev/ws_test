c2383d11a251f1ff03e848099f784582e497311e
Konstantin Zolotarev <konstantin.zolotarev@gmail.com>
2017-02-19 16:09:22 +0200

    experiments

--- lib/ws_test/client.ex
+++ lib/ws_test/client.ex
@@ -7,27 +7,41 @@ defmodule WsTest.Client do
 
   def recv, do: GenServer.cast(__MODULE__, :recv)
 
-  def pong, do: GenServer.call(__MODULE__, :pong)
+  def text, do: GenServer.cast(__MODULE__, :text)
 
   def init(state) do
     socket = Socket.Web.connect! "echo.websocket.org"
     {:ok, %{state | socket: socket}}
   end
 
-  def handle_cast(:recv, %{socket: socket} = state) do
+  def pong(socket) do
+    socket |> Socket.Web.send!({:pong, ""})
+  end
+
+  def listen(socket) do
     case socket |> Socket.Web.recv! do
       {:text, data} ->
-        IO.puts data
+        IO.puts "text"
+        send self(), {:socket, :text, data}
       {:ping, _} ->
         IO.puts "ping/pong"
-        pong()
+        socket |> pong
     end
+    socket |> listen
+  end
 
+  def handle_cast(:recv, %{socket: socket} = state) do
+    socket |> listen
     {:noreply, %{state | socket: socket}}
   end
 
-  def handle_call(:pong, _from, %{socket: socket} = state) do
-    socket |> Socket.Web.send!({:pong, ""})
-    {:noreply, nil, %{state | socket: socket}}
+  def handle_cast(:text, %{socket: socket} = state) do
+    socket |> Socket.Web.send!({:text, "Some text"})
+    {:noreply, %{state | socket: socket}}
+  end
+
+  def handle_info({:socket, :text, text}, state) do
+    IO.inspect text
+    IO.inspect state
   end
 end
